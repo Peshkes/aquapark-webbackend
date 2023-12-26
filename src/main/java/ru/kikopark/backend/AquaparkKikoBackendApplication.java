e145626ad7368e377446f186b8762d6e44eea5a3
peshkes <peshkesv@gmail.com>
2023-12-26 15:08:10 +0200

    Initial commit

---
+++ src/main/java/ru/kikopark/backend/AquaparkKikoBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.kikopark.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaparkKikoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AquaparkKikoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kikopark/backend/AquaparkKikoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kikopark.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquaparkKikoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
